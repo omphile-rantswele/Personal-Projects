81eab38bc840122528c172496d78d725b6d5b1d9
Omphile Rantswele <70807524+omphile-rantswele@users.noreply.github.com>
2022-09-30 14:15:27 +0200

    Add files via upload

---
+++ BankAccount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace BankAccountLibrary
+{
+    public class BankAccount : IBankAccount
+    {
+        private double balance;
+
+
+        public BankAccount(double amount)
+        {
+            balance = amount;
+        }
+
+
+        public override void Deposit(double amount)
+        {
+            balance += amount;
+            Console.WriteLine("The amount has been deposited.");
+
+
+        }
+
+
+        public override void Withdraw(double amount)
+        {
+            if (amount <= balance)
+            {
+                balance -= amount;
+                Console.WriteLine("The amount has been withdrawed.");
+            }
+        }
+
+
+        public void DisplayCurrentBalance()
+        {
+            Console.WriteLine("The current balance of the account is: {0}", balance);
+        }
+    }
+}
---
+++ IBankAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace BankAccountLibrary
+{
+    public abstract class IBankAccount
+    {
+        public abstract void Withdraw(double amount);
+        public abstract void Deposit(double amount);
+    }
+}
